56317289fd3fe6b6531793f6623983030ad46704
Jaden Lund <lundjaden@gmail.com>
2022-09-03 20:25:10 -0400

    commit tree 8ac2f978923df4e4b37da481d4cf2a33c64b2232

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+require_relative './user'
+class Student < User
+   knowledge = []
 
+    # def learn
+       
+    # end
+    # def initialize knowledge
+    #     @knowledge = knowledge
+    # end
+
+    # def self.knowledge
+    #     knowledge
+    # end
 end
+
---
+++ lib/user.rb
@@ -0,0 +1,6 @@
+require "pry"
+class User 
+    attr_accessor :first_name, :last_name
+   
+end
+# user1 = User.new("jaden", "lund")
